86b332f8bf8d18f923aff69d13a6ea2c937e71dd
Amanda Chang <achang140@my.bcit.ca>
2023-07-10 19:05:57 -0700

    Completed check work order function

--- controllers/it-equipment.js
+++ controllers/it-equipment.js
@@ -61,174 +61,134 @@ module.exports = {
             equip_pickup_date: itPickUpDate
           });
         
-          return { success: true };
+          if (laptopAssetTag) {
+            const laptops = await Laptop.create({
+              it_equip_wo_id: itEquipmentWO.it_equip_wo_id,
+              laptop_asset_tag: laptopAssetTag,
+              laptop_serial_no: laptopSerialNumber,
+              laptop_brand: laptopBrand,
+              laptop_model: laptopModel,
+              other_laptop_brand: otherLaptopBrand,
+              other_laptop_model: otherLaptopModel
+            });
+          }
+          
+          if (monitor1AssetTag) {
+            const monitors1 = await Monitor.create({
+              it_equip_wo_id: itEquipmentWO.it_equip_wo_id,
+              monitor_asset_tag: monitor1AssetTag,
+              monitor_serial_no: monitor1SerialNumber,
+              monitor_brand: monitor1Brand,
+              monitor_model: monitor1Model,
+              other_monitor_brand: otherMonitor1Brand,
+              other_monitor_model: otherMonitor1Model
+            });
+          }
+          
+          if (monitor2AssetTag) {
+            const monitors2 = await Monitor.create({
+              it_equip_wo_id: itEquipmentWO.it_equip_wo_id,
+              monitor_asset_tag: monitor2AssetTag,
+              monitor_serial_no: monitor2SerialNumber,
+              monitor_brand: monitor2Brand,
+              monitor_model: monitor2Model,
+              other_monitor_brand: otherMonitor2Brand,
+              other_monitor_model: otherMonitor2Model
+            });
+          }
+          
+          if (dockingStationAssetTag) {
+            const dockStations = await Docking_Station.create({
+              it_equip_wo_id: itEquipmentWO.it_equip_wo_id,
+              dock_asset_tag: dockingStationAssetTag,
+              dock_brand_model: dockingStationBrandModel,
+              other_dock_brand_model: otherDockBrandModel
+            });
+          }
+          
+          if (adaptorAssetTag) {
+            const adaptors = await Adaptor.create({
+              it_equip_wo_id: itEquipmentWO.it_equip_wo_id,
+              adaptor_asset_tag: adaptorAssetTag,
+              adaptor_brand_model: adaptorBrandModel,
+              other_adaptor_brand_model: otherAdaptorBrandModel
+            });
+          }
+          
+          if (itEquipmentWO && itEquipmentWO.it_equip_wo_id != null) {
+            const mice = await Mouse.create({
+              it_equip_wo_id: itEquipmentWO.it_equip_wo_id,
+              mouse_available: mouseAvailable  
+            });
+          }
+          
+          if (itEquipmentWO && itEquipmentWO.it_equip_wo_id != null) {
+            const keyboards = await Keyboard.create({
+              it_equip_wo_id: itEquipmentWO.it_equip_wo_id,
+              keyboard_available: keyboardAvailable
+            });
+          }
+          
+          if (itEquipmentWO && itEquipmentWO.it_equip_wo_id != null) {
+            const locks = await Lock.create({
+              it_equip_wo_id: itEquipmentWO.it_equip_wo_id,
+              lock_available: lockAvailable
+            });
+          }
+          
+          if (ITEquipmentName) {
+          const otherEquips = await Other_Equipment.create({
+            it_equip_wo_id: itEquipmentWO.it_equip_wo_id,
+            other_equip_name: ITEquipmentName,
+            other_equip_asset_tag: equipmentAssetTag,
+            other_equip_serial_no: equipmentSerialNumber,
+            other_equip_brand_model: equipModelBrand
+          });
+        }
+        
+        if (PhoneWO) {
+          const cellPickUpDate = phonePickUpDate ? new Date(phonePickUpDate) : null;
+          
+          cellPhoneWO = await Cell_Phone_WO.create({
+            user_id: users.user_id,
+            phone_work_order: PhoneWO, 
+            phone_pickup_date: cellPickUpDate
+          });
+        }
+        
+        if (phoneAssetTag) {
+          const phones = await Cell_Phone.create({
+            phone_wo_id: cellPhoneWO.phone_wo_id,
+            phone_asset_tag: phoneAssetTag,
+            phone_imei: phoneIMEI,
+            phone_no: phoneNumber,
+            phone_brand: phoneBrand,
+            phone_model: phoneModel,
+            other_phone_brand: otherPhoneBrand,
+            other_phone_model: otherPhoneModel
+          });
+        }
+        
+        if (notes && notes.trim() !== "") {
+          const comment = await Note.create({
+            user_id: users.user_id,
+            note_info: notes 
+          });
+        }
+        return { success: true };
+        // res.send("Data successfully added to the database!");
+        // res.json({ message: "Data successfully added to the database!" });
         } else {
           return { success: false };
         }
-
-        if (laptopAssetTag) {
-          const laptops = await Laptop.create({
-            it_equip_wo_id: itEquipmentWO.it_equip_wo_id,
-            laptop_asset_tag: laptopAssetTag,
-            laptop_serial_no: laptopSerialNumber,
-            laptop_brand: laptopBrand,
-            laptop_model: laptopModel,
-            other_laptop_brand: otherLaptopBrand,
-            other_laptop_model: otherLaptopModel
-          });
-        }
-        
-        if (monitor1AssetTag) {
-          const monitors1 = await Monitor.create({
-            it_equip_wo_id: itEquipmentWO.it_equip_wo_id,
-            monitor_asset_tag: monitor1AssetTag,
-            monitor_serial_no: monitor1SerialNumber,
-            monitor_brand: monitor1Brand,
-            monitor_model: monitor1Model,
-            other_monitor_brand: otherMonitor1Brand,
-            other_monitor_model: otherMonitor1Model
-          });
-        }
-        
-        if (monitor2AssetTag) {
-          const monitors2 = await Monitor.create({
-            it_equip_wo_id: itEquipmentWO.it_equip_wo_id,
-            monitor_asset_tag: monitor2AssetTag,
-            monitor_serial_no: monitor2SerialNumber,
-            monitor_brand: monitor2Brand,
-            monitor_model: monitor2Model,
-            other_monitor_brand: otherMonitor2Brand,
-            other_monitor_model: otherMonitor2Model
-          });
-        }
-        
-        if (dockingStationAssetTag) {
-          const dockStations = await Docking_Station.create({
-            it_equip_wo_id: itEquipmentWO.it_equip_wo_id,
-            dock_asset_tag: dockingStationAssetTag,
-            dock_brand_model: dockingStationBrandModel,
-            other_dock_brand_model: otherDockBrandModel
-          });
-        }
-        
-        if (adaptorAssetTag) {
-          const adaptors = await Adaptor.create({
-            it_equip_wo_id: itEquipmentWO.it_equip_wo_id,
-            adaptor_asset_tag: adaptorAssetTag,
-            adaptor_brand_model: adaptorBrandModel,
-            other_adaptor_brand_model: otherAdaptorBrandModel
-          });
-        }
-        
-        if (itEquipmentWO && itEquipmentWO.it_equip_wo_id != null) {
-          const mice = await Mouse.create({
-            it_equip_wo_id: itEquipmentWO.it_equip_wo_id,
-            mouse_available: mouseAvailable  
-          });
-        }
-        
-        if (itEquipmentWO && itEquipmentWO.it_equip_wo_id != null) {
-          const keyboards = await Keyboard.create({
-            it_equip_wo_id: itEquipmentWO.it_equip_wo_id,
-            keyboard_available: keyboardAvailable
-          });
-        }
-        
-        if (itEquipmentWO && itEquipmentWO.it_equip_wo_id != null) {
-          const locks = await Lock.create({
-            it_equip_wo_id: itEquipmentWO.it_equip_wo_id,
-            lock_available: lockAvailable
-          });
-        }
-        
-        if (ITEquipmentName) {
-        const otherEquips = await Other_Equipment.create({
-          it_equip_wo_id: itEquipmentWO.it_equip_wo_id,
-          other_equip_name: ITEquipmentName,
-          other_equip_asset_tag: equipmentAssetTag,
-          other_equip_serial_no: equipmentSerialNumber,
-          other_equip_brand_model: equipModelBrand
-        });
-      }
-      
-      if (PhoneWO) {
-        const cellPickUpDate = phonePickUpDate ? new Date(phonePickUpDate) : null;
-        
-        cellPhoneWO = await Cell_Phone_WO.create({
-          user_id: users.user_id,
-          phone_work_order: PhoneWO, 
-          phone_pickup_date: cellPickUpDate
-        });
-      }
-      
-      if (phoneAssetTag) {
-        const phones = await Cell_Phone.create({
-          phone_wo_id: cellPhoneWO.phone_wo_id,
-          phone_asset_tag: phoneAssetTag,
-          phone_imei: phoneIMEI,
-          phone_no: phoneNumber,
-          phone_brand: phoneBrand,
-          phone_model: phoneModel,
-          other_phone_brand: otherPhoneBrand,
-          other_phone_model: otherPhoneModel
-        })
-      }
-      
-      if (notes && notes.trim() !== "") {
-        const comment = await Note.create({
-          user_id: users.user_id,
-          note_info: notes 
-        })
-      }
-      
-      // res.send("Data successfully added to the database!");
-      res.json({ message: "Data successfully added to the database!" });
     } catch (error) {
       console.error("Error adding data to the database:", error);
-      // res.status(500).send(`An error occurred while adding data to the database. ${error}`);
       res.status(500).json({ error: "An error occurred while adding data to the database." });
+      // res.status(500).send(`An error occurred while adding data to the database. ${error}`);
     }
   } else {
-    // res.send("Not added to the database!");
     res.json({ message: "Data not added to the database!" });
+    // res.send("Not added to the database!");
   }
 },
 };
-
-
-
-// if (ITEquipmentWO) {
-  //   const itPickUpDate = ITEquipmentPickUpDate ? new Date(ITEquipmentPickUpDate) : null;
-  
-  //   itEquipmentWO = await IT_Equip_WO.create({
-    //     user_id: users.user_id,
-    //     equip_work_order: ITEquipmentWO,
-    //     equip_pickup_date: itPickUpDate
-    //   });
-    // }
-    
-    // if (ITEquipmentWO) {
-    //   const itPickUpDate = ITEquipmentPickUpDate ? new Date(ITEquipmentPickUpDate) : null;
-      
-    //   // const workOrderExists = await this.checkWorkOrderExists(ITEquipmentWO);
-    //   const workOrderExists = await module.exports.checkWorkOrderExists(ITEquipmentWO);
-    
-    //   if (workOrderExists) {
-    //     res.redirect('/submission-result?success=false');
-    //     // return res.status(200).json({ exists: true, message: 'The work order number is duplicated and cannot be saved to the database!' });
-    //   }
-      
-    //   itEquipmentWO = await IT_Equip_WO.create({
-    //     user_id: users.user_id,
-    //     equip_work_order: ITEquipmentWO,
-    //     equip_pickup_date: itPickUpDate
-    //   }); 
-      
-    //   res.redirect('/submission-result?success=true');
-    //   // return res.status(200).json({ exists: false, itEquipmentWO, message: 'Data saved successfully!' });
-      
-    // } else {
-    //   // Handle the case when ITEquipmentWO is not provided
-    //   res.redirect('/submission-result?success=false');
-    //   // res.status(400).json({ error: 'Missing work order number' });
-    // }
