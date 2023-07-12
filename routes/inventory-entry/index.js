469420712f37aac49517dc6cadfd7b493b917a8c
Amanda Chang <achang140@my.bcit.ca>
2023-07-11 23:22:27 -0700

    Updated submission-result.pug and routes

--- routes/inventory-entry/index.js
+++ routes/inventory-entry/index.js
@@ -22,11 +22,11 @@ router.post('/check-work-order', async (req, res) => {
 
 router.post('/check-phone-work-order', async (req, res) => {
   const { PhoneWO } = req.body;
-
+  
   try {
     // Check if the work order number already exists
     const phoneWorkOrderExists = await ITEquipment.checkPhoneWorkOrderExists(PhoneWO);
-
+    
     res.status(200).json({ exists: phoneWorkOrderExists });
   } catch (error) {
     console.error(error);
@@ -34,91 +34,19 @@ router.post('/check-phone-work-order', async (req, res) => {
   }
 })
 
-// router.post('/', async (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const { itEquipmentSuccess, phoneSuccess } = await ITEquipment.saveDataToDB(req, res);
+    const itSuccess = itEquipmentSuccess ? 'true' : 'false';
+    const pSuccess = phoneSuccess ? 'true' : 'false';
+    return res.render('submission-result', { itEquipmentSuccess: itSuccess, phoneSuccess: pSuccess });
+  } catch (error) {
+    console.error(error);
+    return res.redirect('/submission-result?itEquipmentSuccess=false&phoneSuccess=false');
+  }
+});
 
-//   try {
-//     // Check if the work order number already exists
-//     const { itEquipmentSuccess, phoneSuccess } = await ITEquipment.saveDataToDB(req, res);
-
-//     if (itEquipmentSuccess && phoneSuccess) {
-//       return res.redirect('/submission-result?success=true');
-//     } else {
-//       return res.redirect('/submission-result?success=false');
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     return res.redirect('/submission-result?success=false');
-//   }
-// });
-
-// router.post('/', async (req, res) => {
-//   try {
-//     // Bolean returned to check whether the work order number entered is already in the database 
-//     const { itEquipmentSuccess, phoneSuccess } = await ITEquipment.saveDataToDB(req, res);
-//     console.log(itEquipmentSuccess); 
-//     return res.redirect(`/submission-result?itEquipmentSuccess=${itEquipmentSuccess}&phoneSuccess=${phoneSuccess}`);
-//   } catch (error) {
-//     console.error(error);
-//     return res.redirect('/submission-result?itEquipmentSuccess=false&phoneSuccess=false');
-//   }
-// });
-
-// router.post('/', async (req, res) => {
-//   try {
-//     // Bolean returned to check whether the work order number entered is already in the database 
-//     const { itEquipmentSuccess, phoneSuccess } = await ITEquipment.saveDataToDB(req, res);
-//     console.log(itEquipmentSuccess);
-//     const itSuccess = itEquipmentSuccess.toString();
-//     const pSuccess = phoneSuccess.toString();
-//     return res.redirect(`/submission-result?itEquipmentSuccess=${itSuccess}&phoneSuccess=${pSuccess}`);
-//   } catch (error) {
-//     console.error(error);
-//     return res.redirect('/submission-result?itEquipmentSuccess=false&phoneSuccess=false');
-//   }
-// });
-
-// router.post('/', async (req, res) => {
-//   try {
-//     // Boolean returned to check whether the work order number entered is already in the database 
-//     const { itEquipmentSuccess, phoneSuccess } = await ITEquipment.saveDataToDB(req, res);
-//     console.log(itEquipmentSuccess);
-//     return res.redirect(`/submission-result?itEquipmentSuccess=${itEquipmentSuccess}&phoneSuccess=${phoneSuccess}`);
-//   } catch (error) {
-//     console.error(error);
-//     return res.redirect('/submission-result?itEquipmentSuccess=false&phoneSuccess=false');
-//   }
-// });
-
-// router.post('/', async (req, res) => {
-//   try {
-//     const { itEquipmentSuccess, phoneSuccess } = await ITEquipment.saveDataToDB(req, res);
-//     const itSuccess = itEquipmentSuccess.toString();
-//     const pSuccess = phoneSuccess.toString(); 
-//     // const itSuccess = itEquipmentSuccess ? 'true' : 'false';
-//     // const pSuccess = phoneSuccess ? 'true' : 'false';
-//     console.log(itSuccess);
-//     console.log(pSuccess); 
-//     return res.redirect(`/submission-result?itEquipmentSuccess=${itSuccess}&phoneSuccess=${pSuccess}`);
-//   } catch (error) {
-//     console.error(error);
-//     return res.redirect('/submission-result?itEquipmentSuccess=false&phoneSuccess=false');
-//   }
-// });
-
-
-// router.post('/', async (req, res) => {
-//   try {
-//     let { itEquipmentSuccess, phoneSuccess } = await ITEquipment.saveDataToDB(req, res);
-//     itEquipmentSuccess = itEquipmentSuccess ? 'true' : 'false';
-//     phoneSuccess = phoneSuccess ? 'true' : 'false';
-//     console.log(itEquipmentSuccess);
-//     console.log(phoneSuccess); 
-//     return res.render(`/submission-result?itEquipmentSuccess=${itEquipmentSuccess}&phoneSuccess=${phoneSuccess}`);
-//   } catch (error) {
-//     console.error(error);
-//     return res.render(`/submission-result?itEquipmentSuccess=false&phoneSuccess=false`);
-//   }
-// });
+module.exports = router;
 
 
 // router.post('/', async (req, res) => {
@@ -137,33 +65,3 @@ router.post('/check-phone-work-order', async (req, res) => {
 // const savedData = await ITEquipment.saveDataToDB(req, res);
 // res.status(200).json({ exists: false, savedData, message: 'Form submitted successfully!' });
 // res.status(500).json({ error: 'An error occurred' });
-
-
-// router.post('/', async (req, res) => {
-//   try {
-//     const { itEquipmentSuccess, phoneSuccess } = await ITEquipment.saveDataToDB(req, res);
-//     return res.render('submission-result', { itEquipmentSuccess, phoneSuccess });
-//   } catch (error) {
-//     console.error(error);
-//     return res.redirect('/submission-result?itEquipmentSuccess=false&phoneSuccess=false');
-//   }
-// });
-
-// Inside ITEquipment.js or routes.js
-
-router.post('/', async (req, res) => {
-  try {
-    const { itEquipmentSuccess, phoneSuccess } = await ITEquipment.saveDataToDB(req, res);
-    const itSuccess = itEquipmentSuccess.toString();
-    const pSuccess = phoneSuccess.toString(); 
-    console.log(itEquipmentSuccess);
-    console.log(phoneSuccess);
-    return res.redirect(`/submission-result?itEquipmentSuccess=${itSuccess}&phoneSuccess=${pSuccess}`);
-  } catch (error) {
-    console.error(error);
-    return res.redirect('/submission-result?itEquipmentSuccess=false&phoneSuccess=false');
-  }
-});
-
-
-module.exports = router;
